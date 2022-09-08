6d26b5f02aba0ca25e6aea0e2b2287fc7ee7d419
Jasurbek <jasurisoqov685@gmail.com>
2022-09-08 14:31:00 +0500

    all category films

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -16,6 +16,27 @@ const Main = ({ user }) => {
         movie_request={request.fetchNetflixOriginals}
       />
       <Films title={"Trending now"} movie_request={request.fetchTrending} />
+      <Films title={"Top Rated"} movie_request={request.fetchTopRated} />
+      <Films
+        title={"Action movies"}
+        movie_request={request.fetchActionMovies}
+      />
+      <Films
+        title={"Comedy movies"}
+        movie_request={request.fetchComedyMovies}
+      />
+      <Films
+        title={"Horror movies"}
+        movie_request={request.fetchHorrorMovies}
+      />
+      <Films
+        title={"Romance movies"}
+        movie_request={request.fetchRomanceMovies}
+      />
+      <Films
+        title={"Documentaries"}
+        movie_request={request.fetchDocumentaries}
+      />
     </div>
   );
 };
