beb51b2178e454cf77070df1f36cb6ff8117d04f
Jasurbek <jasurisoqov685@gmail.com>
2022-09-09 13:25:00 +0500

    create films file and import

---
+++ src/Components/Films.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import { Tooltip } from "@mui/material";
+
+import axios from "../Baza/axios";
+import request from "../Baza/request";
+
+const Films = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Films;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 
 import Navbar from "../Layouts/Navbar";
 import Banner from "./Banner";
+import Films from "./Films";
 
 const Main = ({ user }) => {
   return (
     <div>
       <Navbar user={user} />
       <Banner />
+      <Films />
     </div>
   );
 };
