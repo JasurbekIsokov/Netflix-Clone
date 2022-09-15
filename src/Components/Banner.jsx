345dbc67deb9726a1757e1886cab7e0853690bd0
Jasurbek <jasurisoqov685@gmail.com>
2022-09-15 13:25:00 +0500

    fetchMovie

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../Baza/axios";
+import request from "../Baza/request";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  const fetchMovie = async () => {
+    const responseData = await axios.get(request.fetchNetflixOriginals);
+    console.log(responseData);
+  };
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
   return (
     <div>
       <h1></h1>
