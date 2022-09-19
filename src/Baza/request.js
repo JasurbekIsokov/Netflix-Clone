604df1c228531d3bbc5bbbd7c385de5fefb657d6
Jasurbek <jasurisoqov685@gmail.com>
2022-09-19 13:15:00 +0500

    create request metod

--- src/Baza/request.js
+++ src/Baza/request.js
@@ -1,1 +1,15 @@
 const API_KEY = "a976a4321a1550222c217c1ac3741aa3";
+
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&languages=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  trailerQuery: `/videos?api_key=${API_KEY}`,
+};
+
+export default request;
