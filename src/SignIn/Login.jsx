81344c02e6c0e76141c9c86f277f2e3000389ff3
Jasurbek <jasurisoqov685@gmail.com>
2022-09-20 14:00:00 +0500

    navbar and login page style

--- src/SignIn/Login.jsx
+++ src/SignIn/Login.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { signInWithGoogle } from "../Firebase/config";
 
+import logo from "../assets/logo.png";
+
 const Login = () => {
   return (
-    <div>
+    <div className="login">
+      <img src={logo} alt="logo" loading="lazy" /> <br />
       <button className="loginBtn" onClick={signInWithGoogle}>
         Sign in with goole
       </button>
